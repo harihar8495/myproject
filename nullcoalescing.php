56d2f1b8df0dac912d0266160378845379ccf4bd
harihar8495 <harithangavelu08@gmail.comcleargit config --global user.email harithangavelu08@gmail.com>
2022-03-02 18:02:30 +0530

    traits example

--- nullcoalescing.php
+++ nullcoalescing.php
@@ -226,3 +226,21 @@ class Tiger extends Animals{
 }
 $tiger = new Animals("tiger");
 echo $tiger->intro();
+
+
+//traits
+trait communicate{
+    public function telegram(){
+        echo "message sent";
+    }
+}
+class Dial{
+    use communicate;
+}
+class Contact{
+    use communicate;
+}
+$dial = new Dial();
+$dial->telegram();
+$contact = new Contact();
+$contact->telegram();
