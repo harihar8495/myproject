951c2212fc76b6ba738ba64dc5619af48c5982e2
harihar8495 <harithangavelu08@gmail.comcleargit config --global user.email harithangavelu08@gmail.com>
2022-03-03 12:34:11 +0530

    polymorphism example

---
+++ polymorphism.php
@@ -0,0 +1,28 @@
+<?php
+interface Animals{
+    public function animalInfo();
+}
+class Rat implements Animals{
+    public function animalInfo(){
+        echo "smallest animal";
+    }
+}
+class Elephant implements Animals{
+    public function animalInfo(){
+        echo "Biggest Mammal";
+    }
+}
+class Giraffe implements Animals{
+    public function animalInfo(){
+        echo "Tallest Animal";
+    }
+}
+
+$giraffe = new Giraffe();
+echo $giraffe->animalInfo();
+
+/*
+output:
+Tallest Animal
+*/
+
