8f078b8ccc4207c85529119333063a0da1298c4f
harihar8495 <harithangavelu08@gmail.comcleargit config --global user.email harithangavelu08@gmail.com>
2022-03-03 14:35:24 +0530

    iterator example

--- iterator.php
+++ iterator.php
@@ -1,8 +1,4 @@
-<?php
-function getIterable(iterable $fruit){
-    foreach($fruit as $fruits){
-        print_r($fruits);
-    }
-}
-$berries = ['cherry','strawberry','mango','orange'];
-getIterable($berries);
+/*
+* output:
+* cherrystrawberrymangoorange
+*/
