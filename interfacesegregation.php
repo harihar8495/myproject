3b1d9433c3ae4f826b1d3ff432c215f8a3d2c279
harihar8495 <harithangavelu08@gmail.comcleargit config --global user.email harithangavelu08@gmail.com>
2022-03-03 18:04:42 +0530

    interfacesegregation and dependancy inversion

---
+++ interfacesegregation.php
@@ -0,0 +1,32 @@
+<?php
+interface Vehicles{
+    public function vehicleName();
+}
+interface Owner{
+    public function ownerName();
+}
+class Car implements Vehicles{
+
+    public function vehicleName(){
+        echo "Four Wheeler";
+    }
+    public function OwnerName(){
+        echo "selva";
+    }
+}
+class Bike implements Vehicles{
+    public function vehicleName(){
+        echo "Two-Wheeler";
+    }
+}
+
+$car = new Car;
+echo $car->vehicleName();
+echo "<br>";
+echo $car->OwnerName();
+
+/*
+ * output:
+ * Four Wheeler
+ * selva
+ */
